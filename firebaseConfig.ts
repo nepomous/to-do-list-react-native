50110da6b027fd6577baee7b648d998ce1ac6c0d
Fillipe Martins de Oliveira <fillipe_martins@hotmail.com>
2023-06-28 10:36:51 -0300

    update ListScreen

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 // Optionally import the services that you want to use
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 // import {...} from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 // import {...} from "firebase/functions";
@@ -18,7 +18,7 @@ const firebaseConfig = {
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_DB = getFirestore(FIREBASE_APP);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+// export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
--- src/screens/List.tsx
+++ src/screens/List.tsx
@@ -1,14 +1,127 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { FIRESTORE_DB } from "../../firebaseConfig";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Entypo } from "@expo/vector-icons";
 
-// import { Container } from './styles';
+export interface Todo {
+  title: string;
+  done: boolean;
+  id: string;
+}
 
-const List: React.FC = () => {
+const List: React.FC = ({ navigation }: any) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState("");
+
+  useEffect(() => {
+    const todoRef = collection(FIRESTORE_DB, "todos");
+
+    const subscriber = onSnapshot(todoRef, {
+      next: (snapshot) => {
+        console.log("UPDATED?");
+        const todos: Todo[] = [];
+        snapshot.docs.forEach((doc) => {
+          todos.push({
+            id: doc.id,
+            ...doc.data(),
+          } as Todo);
+        });
+        setTodos(todos);
+      },
+    });
+    return () => subscriber();
+  }, []);
+
+  const addTodo = async () => {
+    const doc = await addDoc(collection(FIRESTORE_DB, "todos"), {
+      title: todo,
+      done: false,
+    });
+    console.log("add Todo?", doc);
+    setTodo("");
+  };
+
+  const renderTodo = ({ item }: any) => {
+    const toggleDone = async () => {};
+
+    const deleteItem = async () => {};
+    return (
+      <View>
+        <TouchableOpacity onPress={toggleDone}>
+          {item.done && <Ionicons name="md-checkmark-circle" />}
+          {!item.done && <Entypo name="circle" size={24} color="black" />}
+          <Text>{item.title}</Text>
+        </TouchableOpacity>
+        <Ionicons
+          name="trash-bin-outline"
+          size={24}
+          color={"red"}
+          onPress={deleteItem}
+        />
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>List</Text>
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add new Todo"
+          onChangeText={(text: string) => setTodo(text)}
+          value={todo}
+        />
+      </View>
+      <Button
+        onPress={() => addTodo()}
+        title="Add To-do"
+        disabled={todo === ""}
+      />
+      {todos.length > 0 && (
+        <View>
+          <FlatList
+            data={todos}
+            renderItem={(item) => renderTodo(item)}
+            keyExtractor={(todo: Todo) => todo.id}
+          />
+        </View>
+      )}
+      {/* <View>
+        {todos.map((todo) => (
+          <Text key={todo.id}>{todo.title}</Text>
+        ))}
+      </View> */}
     </View>
   );
 };
 
 export default List;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+  },
+  form: {
+    marginVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+});
